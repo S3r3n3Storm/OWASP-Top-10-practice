c0c22b0e8037c277e622f642d1214b51325bd336
DNikolaev <nikolaev@a-vsn.ru>
2018-03-20 11:38:12 +0300

    Delete duble html-title in files

--- A8.CSRF/user.com/View/main_page.php
+++ A8.CSRF/user.com/View/main_page.php
@@ -2,7 +2,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-	  <title>Whitesquare</title>
     <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
 	  <link rel="stylesheet" href="css/styles.css" type="text/css">
     <title>CSRF</title>
