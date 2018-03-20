ef586196855cebdc80f5283e73ea8454a6d9cebb
DNikolaev <nikolaev@a-vsn.ru>
2018-03-20 12:21:05 +0300

    Change html tag

--- A8.CSRF/user.com/View/logged_user.php
+++ A8.CSRF/user.com/View/logged_user.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8">
     <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
